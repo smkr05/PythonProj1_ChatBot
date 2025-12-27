a73ab7f3718db28f419cd02149d182f867723f15
shabab <shabab.r1@outlook.com>
2025-12-27 02:47:46 +0000

    Added code to convert data to documents if location doesn't exist

--- vector.py
+++ vector.py
@@ -9,3 +9,18 @@ embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
 db_location = "./chrome_langchain_db"
 add_documents = not os.path.exists(db_location)
+
+if add_documents:
+    documents = []
+    ids = []
+
+    for i, row in df.iterrows():
+        document = Document(
+            page_content=row["Title"] + " " + row["Review"],
+            metadata={"rating": row["Rating"], "date": row["Date"]},
+            id=str(i)
+        )
+        ids.append(str(i))
+        documents.append(document)
+
+
