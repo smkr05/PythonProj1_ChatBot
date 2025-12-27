7abea8f170d7bee638ae26f9a368c5ed356bdeba
shabab <shabab.r1@outlook.com>
2025-12-27 02:27:35 +0000

    Creating a Vector database

---
+++ vector.py
@@ -0,0 +1,8 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+
+df = pd.read_csv("realistic_restaurant_reviews.csv")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
